562944997826f6ac24eb3aeafa7fae3ecdb43a3e
Scott Burns <scott.s.burns@gmail.com>
2016-03-13 06:33:05 -0500

    Add tasks

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # deployment-api-toy
 Toy repo to tinker with the Github Deployment API
+
+## Setup
+
+- Create a [Personal Access Token](https://help.github.com/articles/creating-an-access-token-for-command-line-use/)
+  - This token only needs the `repo_deployment` scope.
+- Store the token in the `GITHUB_DEPLOYMENT_API_TOKEN` environment variable
+- Get a python virtualenv going
+- `pip install -r requirements.txt`
+
+## What's going on here
+
+The interesting tidbis are in `tasks.py`. Using [invoke](http://pyinvoke.org), this file provides a
+deployment command run as so:
+
+```
+$ invoke deploy
+```
+
+This command creates a [deployment](https://developer.github.com/v3/repos/deployments/) through Github's
+API with various extra bits of information.
+
---
+++ tasks.py
@@ -0,0 +1,50 @@
+import os
+from warnings import warn
+import json
+
+from invoke import task, run
+import requests
+
+ENDPOINT = 'https://api.github.com/repos/sburns/deployment-api-toy/deployments'
+
+
+@task
+def deploy(task='deploy', env='production', desc='my deploy'):
+    create_deployment(rev_parse(), task=task, environment=env, description=desc)
+    # And now you might actually deploy something
+
+
+def create_deployment(ref, task='deploy', auto_merge=True, payload=None,
+        environment='production', description=''):
+    data = {
+        'ref': ref,
+        'task': task,
+        'auto_merge': auto_merge,
+        'payload': json.dumps(payload) if payload else "",
+        'environment': environment,
+        'description': description,
+    }
+    session = prepare_session()
+    return session.post(ENDPOINT, json=data)
+
+
+def get_token():
+    oauth_token = os.environ.get('GITHUB_DEPLOYMENT_API_TOKEN', None)
+    if not oauth_token:
+        warn('''Unauthenticated API calls are rate-limited.
+
+Store a Personal Access Token in GITHUB_DEPLOYMENT_API_TOKEN''')
+    return oauth_token
+
+
+def prepare_session():
+    token = get_token()
+    session = requests.Session()
+    if token:
+        session.headers['Authorization'] = 'token {}'.format(token)
+    return session
+
+
+def rev_parse(ref='HEAD'):
+    return run('git rev-parse {}'.format(ref)).stdout.strip()
+
